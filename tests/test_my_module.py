174d588fe4dcb7e4093694e414918ac8ea6ad3e0
samanttanvi <tanvisamant@goaelectronics.co.in>
2025-03-08 11:55:38 +0530

    Testcases

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -3,9 +3,12 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from src.my_module import add, subtract
+from src.my_module import add, subtract,Multiply
 
 def test_add():
     assert add(2, 3) == 5
 def test_subtract():
     assert subtract(5, 3) == 2
+
+def test_mutiply():
+    assert Multiply(4, 2)==8
